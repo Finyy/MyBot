fba94b0c02dedc4454b7fb81c50f8e0a4de69b8b
Finy <jos666@qq.com>
2015-12-31 12:08:17 +0800

    update

--- kunlun缩放.bat
+++ kunlun缩放.bat
@@ -1,14 +1,3 @@
 @ECHO OFF
-::Set the working directory
-set "workingdir=%~dp0"
-::Switch to working directory
-
-
-title kunlun coc 缩进修复
-echo kunlun coc 缩进修复
-
-
-copy com.supercell.clashofclans.kunlun.cfg C:\ProgramData\BlueStacks\UserData\InputMapper\
-
-echo 完成
+copy com.supercell.clashofclans.kunlun.cfg C:\ProgramData\BlueStacks\UserData\InputMapper\&&echo 完成
 pause >nul
